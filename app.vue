51d484522033125bd7e816503d5b775ecf5fc7d9
zulfidly <zulfidly@gmail.com>
2023-12-17 18:45:26 +0800

    chore: nav menu styling

--- app.vue
+++ app.vue
@@ -6,21 +6,6 @@
 
 
 <script setup>
-const route = useRoute()
-const router = useRouter()
-console.log(router.getRoutes());
-const navItem = router.getRoutes()
-let navTitles = []
-let group = []
-navItem.forEach((x)=> {
-  if(x.path === '/') return
-  
-  navTitles.push(x.meta)
-  group.push(x.meta.group)
-})
-console.log(navTitles);
-console.log(Array.from(new Set(group)))
 
-let temp = new Set()
 
 </script>
---
+++ components/NavItems.vue
@@ -0,0 +1,66 @@
+<template>
+    <v-list>
+        <v-list-item to="/" title="Home" prepend-icon="mdi-home"></v-list-item>
+
+        <v-list-group in item value="subTitle">
+            <template v-slot:activator="{ props }">
+                <v-list-item
+                    v-bind="props"
+                    prepend-icon="mdi-account-circle"
+                    title="BNM"
+                ></v-list-item>
+            </template>
+            <v-list-item 
+                to="/bnm/otherIndicators/msb51" 
+                title="Other Indicators" 
+            />
+
+            <v-list-group>
+                <template v-slot:activator="{ props }">
+                    <v-list-item 
+                        title="Rates and Volume" 
+                        v-bind="props" 
+                    />
+                </template>
+                <v-list-item 
+                    title="Exchange Rates" 
+                    to="/bnm/ratesAndVolume/ExchangeRates" 
+                />
+                <v-list-item 
+                    to="/bnm/ratesAndVolume/FinancialConsumerAlert" 
+                    title="Financial Consumer Alert" 
+                />
+            </v-list-group>
+
+            <v-list-group>
+                <template v-slot:activator="{ props }">
+                    <v-list-item 
+                        title="Payment Statistics" 
+                        v-bind="props" 
+                    />
+                </template>
+                <v-list-item 
+                    to="/bnm/paymentStatistic/BasicPaymentIndicator" 
+                    title="Basic Payment Indicator" 
+                />
+            </v-list-group>
+
+            <v-list-group>
+                <template v-slot:activator="{ props }">
+                    <v-list-item 
+                        title="Other Indicators" 
+                        v-bind="props" 
+                    />
+                </template>
+                <v-list-item 
+                    to="/bnm/paymentStatistic/BasicPaymentIndicator" 
+                    title="Basic Payment Indicator" 
+                />
+            </v-list-group>
+        </v-list-group>
+    </v-list>
+</template>
+
+<script setup>
+
+</script>
